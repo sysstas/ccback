f62f83e90aa001a0b4dbf36f462f6dbf240e5721
Stanislav Sysoiev <sysstas@gmail.com>
2018-04-22 23:19:13 +0300

    email fix

--- controllers/make-order.controller.js
+++ controllers/make-order.controller.js
@@ -62,16 +62,16 @@ async function makeOrder(req, res) {
   // sending email to user 
   let masterName = req.body.masterName;
   let dateMsg = req.body.dateMsg;
-  let startTime = req.body.busyObj.time[0];
-  let duration = req.body.busyObj.time.length
+  let startTime = busyObj.time[0];
+  let duration = busyObj.time.length;
 
   var msg = {
     to: userEmail,
     from: 'noreply@cc.com',
     subject: 'Clockwise Clockwork master order',
-    text: 'Hello, '+userName+'.&#13;&#10; Thank you for order.'+'\n'+' Master '+masterName+' will come to you at '+ startTime+''+dateMsg+' and will repear your clock in about '+duration+' hours',
+    text: '<strong>Hello, '+userName+'.&#13;&#10; Thank you for order.'+'\n'+' Master '+masterName+' will come to you at '+ startTime+' '+dateMsg+' and will repear your clock in about '+duration+' hours</strong>',
     html: '<strong>Visit our <a href="http://ec2-34-244-145-145.eu-west-1.compute.amazonaws.com/">site</a> again if you have another clock to repear</strong>'
   }
-  console.log("message send")
+  console.log(msg)
   sgMail.send(msg)
 }

004d5587343b281896ad071b78f727ec94c365df
Stanislav Sysoiev <sysstas@gmail.com>
2018-06-05 22:57:56 +0300

    clients moved to sql

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -2,15 +2,60 @@ var express = require('express')
 var router = express.Router()
 var Client = require('../models/Client')
 
-// router.get('/', getAllClients);
-// router.post('/', createNewClient);
-// router.put('/:id', editClient);
-// router.delete('/:id', deleteClient);
+var mysql = require('mysql');
+var db_config = {
+	host     : 'localhost',
+	port : '3306',
+	user     : 'root',
+	password : '',
+	database: 'ccdb'
+  };
+
+  var connection;
+
+  function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
+
+router.get('/', getAllClients);
+router.post('/', createNewClient);
+router.put('/:id', editClient);
+router.delete('/:id', deleteClient);
 
 module.exports = router;
 
 // Functions
 
+async function getAllClients(req, res) {
+	try {
+		await connection.query("SELECT * FROM clients", function(er, response){
+      if (!er) 
+      res.status(200).send(response)        
+    });	  
+	} catch (error) {
+		console.log(error)    
+		res.sendStatus(500)
+	}  
+}
 
 
 // async function getAllClients(req, res) {
@@ -22,6 +67,25 @@ module.exports = router;
 // 		res.sendStatus(500)
 // 	}  
 // }
+
+async function createNewClient(req, res) {
+  let newMaster = req.body
+	console.log('request: ', newMaster)
+   let sql = `INSERT INTO clients (clientName, clientEmail) 
+   VALUES('${req.body.clientName}','${req.body.clientEmail}')
+   `
+  console.log('sql: ', sql)
+  try {
+    await connection.query(sql, function(er, result){
+        if (!er) 
+        console.log(result)
+        res.status(201).send(result)
+      });	  
+  } catch (error) {
+    console.log(error)    
+    res.sendStatus(500) 
+  }  
+}
 
 // async function createNewClient(req, res) {
 // 	let client = req.body
@@ -40,6 +104,27 @@ module.exports = router;
 // 	}
 // }
 
+async function editClient(req, res) {
+  console.log('request: ', req.body) 
+  let sql =`UPDATE clients 
+  SET clientName = '${req.body.clientName}',
+  clientEmail = '${req.body.clientEmail}'
+  WHERE ID = ${req.params.id}
+  ` 
+  console.log('sql: ', sql)
+  console.log('ID: ', req.params.id)
+  try {
+    await connection.query(sql, function(er, result){
+        if (!er) 
+        console.log(result)
+        return res.status(200).send({result});
+      });	  
+  } catch (error) {
+    console.log(error)    
+    res.sendStatus(500) 
+  } 	
+}
+
 // async function editClient(req, res) {
 // 	await Client.findByIdAndUpdate(req.params.id,
 // 		req.body,
@@ -54,6 +139,21 @@ module.exports = router;
 // 		}
 // 	)
 // }
+
+async function deleteClient(req, res) {
+  console.log('ID: ', req.params.id)
+  let sql = "DELETE FROM clients WHERE ID = '"+req.params.id+"'" 
+  try {
+    await connection.query(sql, function(er, result){
+        if (!er) 
+        console.log(result)
+        return res.status(204).send(result);
+      });	  
+  } catch (error) {
+    console.log(error)    
+    res.sendStatus(500) 
+  }  	
+}
 
 // async function deleteClient(req, res) {
 // 	await Client.findByIdAndRemove( req.params.id, (err, result) => {  
--- migrationBase.sql
+++ migrationBase.sql
@@ -1,2 +1,16 @@
 /// MASTERS
-CREATE TABLE masters ( MasterID int NOT NULL AUTO_INCREMENT, masterName varchar(30), cityID int, masterRating int, PRIMARY KEY (MasterID) );
+CREATE TABLE masters ( 
+    MasterID int NOT NULL AUTO_INCREMENT, 
+    masterName varchar(30) NOT NULL, 
+    cityID int NOT NULL, 
+    masterRating int NOT NULL, 
+    PRIMARY KEY (MasterID) 
+);
+
+/// CLIENTS
+CREATE TABLE clients (
+    ID int NOT NULL AUTO_INCREMENT,
+    clientName varchar(30) NOT NULL,
+    clientEmail varchar(50) NOT NULL UNIQUE,
+    PRIMARY KEY (ID)
+);

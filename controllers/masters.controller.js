f94961c72c8245f2a8a7477fb95ab7afdca8dcd1
Stanislav Sysoiev <sysstas@gmail.com>
2018-07-23 21:10:16 +0300

    Masters correct JWT

--- controllers/masters.controller.js
+++ controllers/masters.controller.js
@@ -15,7 +15,7 @@ router.delete('/:id', deleteMaster);
 module.exports = router;
 
 // Functions
-// Get all masters
+// Get all masters request hendling
 async function getAllMasters(req, res) {
 	try {
      await 
@@ -29,91 +29,167 @@ async function getAllMasters(req, res) {
 	}  
 }
 
-//Create new master
-async function createNewMaster(req, res){
-    console.log('creation request')
-    //console.log(req.body.clientName)
-    try {
-      await 
-      //Checking is user Admin
-      AuthCheck( req.body.token)
-      Master.build({ 
-        masterName: req.body.masterName,
-        cityID: req.body.cityID,
-        masterRating: req.body.masterRating
-      })
-        .save()
-        .then( result => {
-          // you can now access the currently saved task with the variable anotherTask... nice!
-          res.status(201).send(result)
+//Create new master request hendling
+async function createNewMaster(req, res){  
+  try {     
+    console.log('Master creation request')
+    //sending token to decoder
+    let adminCredentials = tokenDecoding(req.body.token);
+    if (adminCredentials) {
+      //if decoded correctly
+      console.log('token decoded correctly')
+      // verifying Admin credentials      
+      if (await verifyAdmin(adminCredentials)) {
+        // if Admin verified Creating new Master
+        console.log('Admin verified')
+        Master.build({ 
+          masterName: req.body.masterName,
+          cityID: req.body.cityID,
+          masterRating: req.body.masterRating
+        })
+          .save()
+          .then( result => {
+          // if successfully saved send status 201
+            res.status(201).send(result)
+          })
+        .catch(error => {
+          // if some errors - throw them to further handle
+          throw error
+        })
+      } else {
+        // if Admin not verified send status 401
+        console.log('Admin not vryfied. Access denied')
+        res.sendStatus(401)
+      }
+    } else {
+      //if token is broken send status 400
+      console.log('token cannot be decoded')
+      res.sendStatus(400) 
+    }
+  // errors hendling send status 500
+  } catch (err) {
+    console.log(err)    
+    res.sendStatus(500) 
+  }  
+}
+  
+//Edit master request hendling
+async function editMaster(req, res){ 
+  try {     
+    console.log('Master edit request')
+    //sending token to decoder
+    let adminCredentials = tokenDecoding(req.body.token);
+    if (adminCredentials) {
+      //if decoded correctly
+      console.log('token decoded correctly')
+      // verifying Admin credentials      
+      if (await verifyAdmin(adminCredentials)) {
+        // if Admin verified editing Master
+        console.log('Admin verified')
+        Master.findById(req.params.id).then( master => {
+          master.update({ 
+            masterName: req.body.masterName,
+            cityID: req.body.cityID,
+            masterRating: req.body.masterRating
+          }).then( result => {
+          // if successfully saved send status 201
+            return res.status(200).send(result);
+          })
         })
         .catch(error => {
-          // Ooops, do some error-handling
-        })        
-    } catch (error) {
-      console.log(error)    
-      res.sendStatus(500) 
-    }  
-  }
-  
-//Edit master
-async function editMaster(req, res){//   
-  console.log('Edit request')
-  //console.log(req.params.id)
-  //console.log(req.body.cityName)
-  try {
-    await 
-    //Checking is user Admin
-    AuthCheck( req.body.token)
-    Master.findById(req.params.id).then( master => {
-      master.update({ 
-        masterName: req.body.masterName,
-        cityID: req.body.cityID,
-        masterRating: req.body.masterRating
-      }).then( result => {
-        return res.status(200).send(result);
-      })
-    })      	  
-  } catch (error) {
-    console.log(error)    
+          // if some errors - throw them to further handle
+          throw error
+        })
+      } else {
+        // if Admin not verified send status 401
+        console.log('Admin not vryfied. Access denied')
+        res.sendStatus(401)
+      }
+    } else {
+      //if token is broken send status 400
+      console.log('token cannot be decoded')
+      res.sendStatus(400) 
+    }
+  // errors hendling send status 500
+  } catch (err) {
+    console.log(err)    
     res.sendStatus(500) 
   }  
 }
 
-//Delete master
-async function deleteMaster(req, res){ 
-  console.log('Delete request')
-  console.log(req.params.id)
-  try {
-    await 
-    //Checking is user Admin
-    AuthCheck( req.body.token)
-    Master.destroy({
-      where: {
-        ID: req.params.id
+//Delete master request hendling
+async function deleteMaster(req, res){
+ 
+  try {     
+    console.log('Master edit request')
+    //sending token to decoder
+    let adminCredentials = tokenDecoding(req.body.token);
+    if (adminCredentials) {
+      //if decoded correctly
+      console.log('token decoded correctly')
+      // verifying Admin credentials      
+      if (await verifyAdmin(adminCredentials)) {
+        // if Admin verified editing Master
+        console.log('Admin verified')
+        Master.destroy({
+          where: {
+            ID: req.params.id
+          }
+        }).then( result => {
+          console.log(result)
+          return res.sendStatus(204)        
+        })
+        .catch(error => {
+          // if some errors - throw them to further handle
+          throw error
+        })
+      } else {
+        // if Admin not verified send status 401
+        console.log('Admin not vryfied. Access denied')
+        res.sendStatus(401)
       }
-    }).then( result => {
-      console.log(result)
-      return res.sendStatus(204);
+    } else {
+      //if token is broken send status 400
+      console.log('token cannot be decoded')
+      res.sendStatus(400) 
+    }
+  // errors hendling send status 500
+  } catch (err) {
+    console.log(err)    
+    res.sendStatus(500) 
+  }  
+}
+
+///////////////HELPER FUNCTIONS////////////////////////////////////////////////////////////
+  
+// Token decoding function
+function tokenDecoding(token) {
+  let payload = {}
+  try {
+    jwt.verify(token,'secret',(err, decoded) => {    
+      console.log('decoded on city: ', decoded)
+      payload.login = decoded.login
+      payload.password = decoded.password    
     })
   } catch (error) {
-    console.log(error)    
-    res.sendStatus(500) 
-  }  
+    return false
+  } 
+  console.log('payload: ', payload)
+  return payload;
 }
 
-
-function AuthCheck(token){
-  let payload = {}
-  jwt.verify(token,'secret',(err, decoded) => {
-    console.log('decoded on city: ', decoded)
-    payload.login = decoded.login
-    payload.password = decoded.password
-  })
-  // If there no such Admin in DB - node will throw an error
-  Admin.findOne({ where: {login: payload.login, password: payload.password} })
-  .then( result => {
-    console.log('login: ', result.dataValues.login)
-    console.log('password: ', result.dataValues.password) 
-  })
+// Verifying Admin function
+async function verifyAdmin(credentials){
+  let isAdmin = await Admin.findOne({ where: {login: credentials.login, password: credentials.password} }).then( result => {
+    // let payload = {}
+    if (result === null) {
+      console.log('access denied')
+      return false
+    } else {
+      console.log('login: ', result.dataValues.login)
+      console.log('password: ', result.dataValues.password) 
+      return true		
+    }
+  })  
+  return isAdmin;
 }

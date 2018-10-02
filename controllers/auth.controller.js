2cde51cb07e48b2ed626b00a223f36b8c14782a6
sysstas <sysstas@gmail.com>
2018-10-02 15:17:32 +0300

    minor changes for heroku

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,19 +11,27 @@ router.post('/', auth)
 module.exports = router
 
 let userData, isCreated, user
+user = null
+isCreated = null
 const payload = {}
 async function auth (req, res) {
   const { login, password, googleToken } = req.body
   try {
+    console.log('************************************************************SERVER received req.body', req.body)
     // If user logiggn in with Google login
-    if (googleToken) {
+    if (googleToken !== null) {
       // Verifying Google token
       await verifyGoogleToken(googleToken)
       // Checking if user exist. If not - creating one
       await checkUserOrCreateNew(userData)
     }
+    if (googleToken === null) {
+      console.log('--------------------googleToken = 0 ')
+      isCreated = null
+      user = null
+    }
     // If user logged in by Google is new we must to add additional information to his account
-    if (isCreated === true) {
+    if (isCreated === true && isCreated !== null) {
       const newuser = await User.findById(user.id)
       const result = await newuser.update({ regToken: 'Registered by Google', isAdmin: 0, isRegistered: 1, userName: userData.userName })
       // console.log(result.get({ plain: true }))
@@ -35,16 +43,20 @@ async function auth (req, res) {
       const token = jwt.sign(payload, 'secret')
       // console.log('Google token', token)
       res.status(200).send({ token: token })
+      
     }
-    if (isCreated === false) {
-      // console.log('User is not created', user)
+    if (isCreated === false && isCreated !== null) {
+      console.log('--isCreated', isCreated)
+      console.log('--user', user)
       payload.isAdmin = user.isAdmin
       payload.isRegistered = user.isRegistered
       payload.regToken = user.regToken
       payload.ID = user.id
       const token = jwt.sign(payload, 'secret')
+      console.log('--------------------User is not created', payload)
       // console.log('Google token', token)
       res.status(200).send({ token: token })
+      
     }
     // If user was logged in by login-password form
     const result = await User.findOne({ where: { userEmail: login } })
@@ -54,8 +66,10 @@ async function auth (req, res) {
       payload.isRegistered = result.dataValues.isRegistered
       payload.regToken = result.dataValues.regToken
       payload.ID = result.dataValues.id
+      console.log('--------------------SERVER classic login', payload)
       const token = jwt.sign(payload, 'secret')
       res.status(200).send({ token: token })
+      
     } else { res.sendStatus(401) }
   } catch (error) {
     res.sendStatus(500)
@@ -64,6 +78,7 @@ async function auth (req, res) {
 
 /// verifyGoogleToken
 async function verifyGoogleToken (token) {
+  console.log('verifyGoogleToken runs')
   const client = new OAuth2Client('902455189500-mpc1v2qsioej6o17e2no0rc122vh40bh.apps.googleusercontent.com')
   async function verify () {
     const ticket = await client.verifyIdToken({
@@ -79,7 +94,7 @@ async function verifyGoogleToken (token) {
 }
 
 async function checkUserOrCreateNew (userData) {
-  console.log('function is running')
+  console.log('checkUserOrCreateNew runs')
   await User.findOrCreate({ where: {
     userEmail: userData.userEmail
   } }).spread((userinfo, created) => {
@@ -87,5 +102,5 @@ async function checkUserOrCreateNew (userData) {
     isCreated = created
     user = userinfo.get({ plain: true })
   })
-  console.log('asdfasdfasdf', isCreated, user)
+  // console.log('asdfasdfasdf', isCreated, user)
 }

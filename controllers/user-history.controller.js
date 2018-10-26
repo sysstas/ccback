e480ec6cc7afb6b544e515640711469af8fda227
sysstas <sysstas@gmail.com>
2018-10-26 15:49:51 +0300

    Client can see his orders

--- controllers/user-history.controller.js
+++ controllers/user-history.controller.js
@@ -3,15 +3,18 @@ const router = express.Router()
 
 const userauth = require('./checkUserAuth.controller')
 const auth = require('./checkAuth.controller')
-const checkUserRegistered = userauth.checkUserAuthenticated
+const validateToken = auth.validateToken
+
+// const checkUserRegistered = userauth.checkUserAuthenticated
 const tokenDecoding = auth.tokenDecoding
+const logger = require('./logger.service')
 
 const Master = require('../models/Master')
 const City = require('../models/City')
 const Order = require('../models/Order')
 const User = require('../models/User')
 
-router.get('/', checkUserRegistered, userOrderHistoryData)
+router.get('/', userOrderHistoryData)
 // router.post('/', registerUser);
 // router.put('/:id', checkRegistered, editCity);
 // router.delete('/:id', checkRegistered, deleteCity);
@@ -21,11 +24,24 @@ module.exports = router
 // Functions
 /// Get  user account data
 async function userOrderHistoryData (req, res) {
+  const tokenPayload = await validateToken(req, res)
+  logger.info(`User email:  ${tokenPayload.email}`)
+  let user
   try {
-    const token = req.header('authorization').split(' ')[1]
-    const payload = tokenDecoding(token)
-    const data = await Order.findAll({ include: [City, Master, User], where: { userID: payload.ID } })
-    return res.status(200).send(data)
+    const data = await User.findOne({ where: { userEmail: tokenPayload.email } }, { attributes: ['id', 'isAdmin', 'isRegistered', 'regToken', 'userEmail', 'userName', 'password'] })
+    if (data) {
+      logger.info(`User data retrieved from api db:  ${data}`)
+      user = data.get({ plain: true })
+      logger.info(`User data:  ${user}`)
+      console.log(user)
+    }
+    const orders = await Order.findAll({ include: [City, Master, User], where: { userID: user.id } })
+    if (orders) {
+      logger.info(`Orders data retrieved from api db:  ${data}`)
+      return res.status(200).send(orders)
+    }
+    throw Error('Something miss in DB')
+
   } catch (error) {
     // console.log('error', error)
     res.sendStatus(500)

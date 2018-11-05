717098623ed01c0c517d3da5841965244712ff09
sysstas <sysstas@gmail.com>
2018-11-05 12:27:24 +0200

    ci changes

--- seeders/20181010072058-user-admin.js
+++ seeders/20181010072058-user-admin.js
@@ -6,18 +6,12 @@ module.exports = {
       {
         userName: 'Admin',
         userEmail: 'admin@example.com',
-        isAdmin: 1,
-        regToken: 1,
-        isRegistered: 1,
-        password: '$2b$10$m2xQWuYPvnPsdBAo1jDYxOD.r95efh.GiGN/Z5xprfGR.O9dyHQfG'
+        isAdmin: 1
       },
       {
         userName: 'Dummy client',
         userEmail: 'Dummy@client.com',
-        isAdmin: 0,
-        regToken: 1,
-        isRegistered: 0,
-        password: null
+        isAdmin: 0
       }
     ], {})
   },

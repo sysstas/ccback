cc0b4cd39146f355abf03a4ece417d216e86e4c7
sysstas <sysstas@gmail.com>
2018-11-12 12:51:34 +0200

    minor

--- helpers/connection.helper.js
+++ helpers/connection.helper.js
@@ -6,15 +6,18 @@ const logger = require('../services/logger.service')
 if (process.env.NODE_ENV === 'test') {
   sequelize = new Sequelize(process.env.DB_CONFIG_TEST, { logging: false })
   logger.info(`CONNECTED TO ${process.env.DB_CONFIG_TEST}`)
+  logger.info(`Test env`)
 }
 
 if (process.env.NODE_ENV === 'dev') {
   sequelize = new Sequelize(process.env.DB_CONFIG_DEV, { logging: false })
   // sequelize = new Sequelize(process.env.DB_CONFIG_DEV, { logging: true })
+  logger.info(`Dev env`)
   logger.info(`CONNECTED TO ${process.env.DB_CONFIG_DEV}`)
 } else {
   logger.info(`CONNECTED TO ${process.env.DB_CONFIG_PROD}`)
   sequelize = new Sequelize(process.env.DB_CONFIG_PROD, { logging: false })
+  logger.info(`Dev env`)
 }
 
 sequelize
